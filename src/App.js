f277b4c21ebf55668101b06458aef944fbdec806
codeinvisible <lamasandesh400@gmail.com>
2021-12-09 11:45:31 +0545

    login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import {
-  getProvider,
-
-} from "zebecprotocol-sdk";
-
+import { useState } from 'react'
+import {getProvier} from './login/Phantom'
 function App() {
-  const clickHandler = () =>{
-    console.log("clicked")
-    getProvider();
+  const [loginMode, setLoginMode] = useState('phantom')
+
+  const changeHandler = (e) => {
+    const {value} = e.target;
+    setLoginMode(value)
   }
-  // getProvider();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (loginMode === "phantom"){
+      const {isPhantom} = getProvier()
+      
+    }
+  }
+  // getProvier()
   return (
     <div className="App">
-      <button onClick={clickHandler}>connect to  Phantom</button>
+      <div className="text-center mt-32">
+        <h1 className="text-5xl font-bold my-3">Welcome to Zeb</h1>
+        <form className='flex flex-col w-1/2 m-auto gap-y-3 items-center' onSubmit={submitHandler}>
+          <select className="py-4 px-10 text-white bg-blue-600 rounded-lg text-lg text-center" name='loginMode' value={loginMode} id="loginMode" onChange={changeHandler}>
+            <option value="phantom" className='text-center'>Connect via Phantom</option>
+            <option value="metamask" className='text-center'>Connect via Metamask</option>
+          </select>
+          <button type='submit' className='inline rounded-lg w-1/5 p-3 bg-blue-800 text-white'>Login</button>
+        </form>
+
+      </div>
+     
     </div>
   );
 }
---
+++ src/login/Phantom.js
@@ -0,0 +1,9 @@
+export const getProvier = () => {
+    if ("solana" in window) {
+        const provider = window.solana;
+        if (provider.isPhantom) {
+          return provider;
+        }
+      }
+      window.open("https://phantom.app/", "_blank");
+}   
